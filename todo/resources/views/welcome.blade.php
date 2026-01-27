9d0792b3dacc28f8fba4b6067bec0e173e1e8e48
Marcos <markitosanches@gmail.com>
2026-01-27 11:40:51 -0500

    Seance 5

---
+++ todo/resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('title', 'Welcome')
+@section('content')
+   <div class="container">
+        <div class="row justify-content-center my-5">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Welcome to {{ config('app.name') }} List App</h1>
+                    </div>
+                    <div class="card-body">
+                        <p class="lead">This is a simple todo list application built with Laravel and Bootstrap.</p>
+                        <p>Get started by creating your first task!</p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="{{route('task.index')}}" class="btn btn-primary">Go to ToDo List</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </div>
+@endsection
